d4fe3393cc33954e80e07f5224b5ce77f2028d97
Minh Tang Ba <tangbamiinh@gmail.com>
2021-05-01 18:53:42 +0700

    Added ownership rules for authorityService

--- src/main/java/com/se2/bankingsystem/domains/Authority/AuthorityService.java
+++ src/main/java/com/se2/bankingsystem/domains/Authority/AuthorityService.java
@@ -1,9 +1,25 @@
 package com.se2.bankingsystem.domains.Authority;
 
-import com.se2.bankingsystem.base.CRUDService;
+import com.se2.bankingsystem.base.crud.CRUDService;
 import com.se2.bankingsystem.domains.Authority.dto.CreateAuthorityDTO;
 import com.se2.bankingsystem.domains.Authority.entity.Authority;
+import com.se2.bankingsystem.domains.User.entity.User;
+import org.springframework.security.core.GrantedAuthority;
 
 public interface AuthorityService extends CRUDService<Authority, Long, CreateAuthorityDTO, CreateAuthorityDTO> {
-    boolean hasCustomerAccountAccess(Long currentUserID, String customerAccountID);
+    boolean hasCustomerAccountOwnerAccess(Long userID, String customerAccountID);
+
+    boolean hasCustomerAccountOwnerAccess(String customerAccountID);
+
+    boolean hasTransactionOwnerAccess(Long userID, Long transactionID);
+
+    boolean hasTransactionOwnerAccess(Long transactionID);
+
+    boolean hasWalletOwnerAccess(Long userID, Long walletID);
+
+    boolean hasWalletOwnerAccess(Long walletID);
+
+    User getPrincipal();
+
+    String getCurrentAuthority();
 }
--- src/main/java/com/se2/bankingsystem/domains/Authority/AuthorityServiceImpl.java
+++ src/main/java/com/se2/bankingsystem/domains/Authority/AuthorityServiceImpl.java
@@ -4,9 +4,16 @@ import com.se2.bankingsystem.domains.Authority.dto.CreateAuthorityDTO;
 import com.se2.bankingsystem.domains.Authority.entity.Authority;
 import com.se2.bankingsystem.domains.CustomerAccount.CustomerAccountRepository;
 import com.se2.bankingsystem.domains.CustomerAccount.entity.CustomerAccount;
+import com.se2.bankingsystem.domains.FakeEWallet.FakeEWalletRepository;
+import com.se2.bankingsystem.domains.FakeEWallet.entity.FakeEWallet;
+import com.se2.bankingsystem.domains.Transaction.TransactionRepository;
+import com.se2.bankingsystem.domains.Transaction.entity.Transaction;
+import com.se2.bankingsystem.domains.User.entity.User;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityNotFoundException;
@@ -18,6 +25,8 @@ public class AuthorityServiceImpl implements AuthorityService {
 
     private final AuthorityRepository authorityRepository;
     private final CustomerAccountRepository customerAccountRepository;
+    private final FakeEWalletRepository fakeEWalletRepository;
+    private final TransactionRepository transactionRepository;
 
     @Override
     public Authority create(CreateAuthorityDTO createAuthorityDTO) {
@@ -53,8 +62,46 @@ public class AuthorityServiceImpl implements AuthorityService {
     }
 
     @Override
-    public boolean hasCustomerAccountAccess(Long currentUserID, String customerAccountID) {
+    public boolean hasCustomerAccountOwnerAccess(Long userID, String customerAccountID) {
         CustomerAccount customerAccount = customerAccountRepository.findById(customerAccountID).orElseThrow(EntityNotFoundException::new);
-        return customerAccount.getCustomer().getId().equals(currentUserID);
+        return customerAccount.getCustomer().getId().equals(userID);
+    }
+
+    @Override
+    public boolean hasCustomerAccountOwnerAccess(String customerAccountID) {
+        return hasCustomerAccountOwnerAccess(getPrincipal().getId(), customerAccountID);
+    }
+
+    @Override
+    public boolean hasTransactionOwnerAccess(Long userID, Long transactionID) {
+        Transaction transaction = transactionRepository.findById(transactionID).orElseThrow(EntityNotFoundException::new);
+        return transaction.getCustomerAccount().getCustomer().getId().equals(userID);
+    }
+
+    @Override
+    public boolean hasTransactionOwnerAccess(Long transactionID) {
+        return hasTransactionOwnerAccess(getPrincipal().getId(), transactionID);
+    }
+
+    @Override
+    public boolean hasWalletOwnerAccess(Long userID, Long walletID) {
+        FakeEWallet fakeEWallet = fakeEWalletRepository.findById(walletID).orElseThrow(EntityNotFoundException::new);
+        return fakeEWallet.getCustomer().getId().equals(userID);
+    }
+
+    @Override
+    public boolean hasWalletOwnerAccess(Long walletID) {
+        return hasWalletOwnerAccess(getPrincipal().getId(), walletID);
+    }
+
+    @Override
+    public User getPrincipal() {
+        return (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    }
+
+    @Override
+    public String getCurrentAuthority() {
+        User currentUser = getPrincipal();
+        return currentUser.getAuthorities().iterator().next().getAuthority();
     }
 }

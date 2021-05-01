6d23a2b42425dbd81d3ecb95bcbf58dd033f3841
Minh Tang Ba <tangbamiinh@gmail.com>
2021-05-01 18:55:18 +0700

    More customer data sample

--- src/main/java/com/se2/bankingsystem/InitData.java
+++ src/main/java/com/se2/bankingsystem/InitData.java
@@ -1,5 +1,6 @@
 package com.se2.bankingsystem;
 
+import com.se2.bankingsystem.config.exception.BankingSystemException;
 import com.se2.bankingsystem.domains.Authority.AuthorityService;
 import com.se2.bankingsystem.domains.Authority.dto.CreateAuthorityDTO;
 import com.se2.bankingsystem.domains.Authority.entity.AuthorityName;
@@ -32,7 +33,7 @@ public class InitData {
         this.userService = userService;
     }
 
-    public void init() {
+    public void init() throws BankingSystemException {
         if (authorityService.findAll().isEmpty()) {
 
             String username = "admin";
@@ -59,16 +60,72 @@ public class InitData {
                 .firstName("Minh")
                 .lastName("Tang Ba")
                 .password("1801040147")
-                .username("tangbamiinh")
-                .email("tangbamiinh@gmail.com")
+                .username("tangbaminh")
+                .email("tangbaminh@gmail.com")
                 .address("Thanh Xuan, Hanoi")
                 .gender(Gender.MALE)
                 .role(AuthorityName.CUSTOMER)
-                .phoneNumber("0904842084")
+                .phoneNumber("0900000001")
+                .build();
+
+            CreateCustomerDTO createCustomerDTO2 = CreateCustomerDTO.builder()
+                .dob(LocalDate.of(2000, 8, 8))
+                .firstName("Duong")
+                .lastName("Nguyen Thi Thuy")
+                .password("1801040052")
+                .username("nguyenthuyduong")
+                .email("nguyenthuyduong@gmail.com")
+                .address("Thai Thinh, Hanoi")
+                .gender(Gender.FEMALE)
+                .role(AuthorityName.CUSTOMER)
+                .phoneNumber("0900000002")
+                .build();
+
+            CreateCustomerDTO createCustomerDTO3 = CreateCustomerDTO.builder()
+                .dob(LocalDate.of(2000, 4, 12))
+                .firstName("Nam")
+                .lastName("Duong Hoang")
+                .password("1801040153")
+                .username("duonghoangnam")
+                .email("duonghoangnam@gmail.com")
+                .address("Thanh Xuan, Hanoi")
+                .gender(Gender.MALE)
+                .role(AuthorityName.CUSTOMER)
+                .phoneNumber("0900000003")
+                .build();
+
+            CreateCustomerDTO createCustomerDTO4 = CreateCustomerDTO.builder()
+                .dob(LocalDate.of(2000, 8, 8))
+                .firstName("Phuong")
+                .lastName("An Thi")
+                .password("1801040169")
+                .username("anthiphuong")
+                .email("anthiphuong@gmail.com")
+                .address("Thanh Xuan, Hanoi")
+                .gender(Gender.FEMALE)
+                .role(AuthorityName.CUSTOMER)
+                .phoneNumber("0900000004")
+                .build();
+
+            CreateCustomerDTO createCustomerDTO5 = CreateCustomerDTO.builder()
+                .dob(LocalDate.of(2000, 8, 8))
+                .firstName("Anh")
+                .lastName("Tran Thi Ngoc")
+                .password("1801040017")
+                .username("tranthingocanh")
+                .email("tranthingocanh@gmail.com")
+                .address("Thanh Xuan, Hanoi")
+                .gender(Gender.FEMALE)
+                .role(AuthorityName.CUSTOMER)
+                .phoneNumber("0900000005")
                 .build();
 
             userService.create(createUserDTO);
             customerService.create(createCustomerDTO);
+            customerService.create(createCustomerDTO2);
+            customerService.create(createCustomerDTO3);
+            customerService.create(createCustomerDTO4);
+            customerService.create(createCustomerDTO5);
 
             testData();
         } else {

7c8dedd5ffd469d8f107c16d186890b37ebaa571
Minh Tang Ba <tangbamiinh@gmail.com>
2021-05-04 19:25:43 +0700

    sửa bọ 2

--- src/main/java/com/se2/bankingsystem/domains/Transaction/sub/WithdrawTransaction/WithdrawTransactionServiceImpl.java
+++ src/main/java/com/se2/bankingsystem/domains/Transaction/sub/WithdrawTransaction/WithdrawTransactionServiceImpl.java
@@ -1,6 +1,8 @@
 package com.se2.bankingsystem.domains.Transaction.sub.WithdrawTransaction;
 
 import com.se2.bankingsystem.config.exception.BankingSystemException;
+import com.se2.bankingsystem.domains.Authority.AuthorityService;
+import com.se2.bankingsystem.domains.Authority.entity.AuthorityName;
 import com.se2.bankingsystem.domains.CustomerAccount.CustomerAccountRepository;
 import com.se2.bankingsystem.domains.CustomerAccount.behaviours.Withdrawable;
 import com.se2.bankingsystem.domains.CustomerAccount.entity.CustomerAccount;
@@ -20,10 +22,12 @@ import java.util.List;
 public class WithdrawTransactionServiceImpl extends AbstractTransactionServiceImpl<WithdrawTransaction, CreateWithdrawTransactionDTO, UpdateWithdrawTransactionDTO> implements WithdrawTransactionService {
 
     private final FakeEWalletRepository fakeEWalletRepository;
+    private final AuthorityService authorityService;
 
-    public WithdrawTransactionServiceImpl(CustomerAccountRepository customerAccountRepository, WithdrawTransactionRepository transactionRepository, ModelMapper modelMapper, FakeEWalletRepository fakeEWalletRepository) {
+    public WithdrawTransactionServiceImpl(CustomerAccountRepository customerAccountRepository, WithdrawTransactionRepository transactionRepository, ModelMapper modelMapper, FakeEWalletRepository fakeEWalletRepository, AuthorityService authorityService) {
         super(customerAccountRepository, transactionRepository, modelMapper);
         this.fakeEWalletRepository = fakeEWalletRepository;
+        this.authorityService = authorityService;
     }
 
     @Override
@@ -37,15 +41,17 @@ public class WithdrawTransactionServiceImpl extends AbstractTransactionServiceIm
         if (!(customerAccount instanceof Withdrawable))
             throw new BankingSystemException("Account type not supported for withdraw transaction");
 
-        FakeEWallet wallet = fakeEWalletRepository.findById(createWithdrawTransactionDTO.getWalletID()).orElseThrow(EntityNotFoundException::new);
+        // If this withdraw transaction was made by a customer
+        // Then the field createWithdrawTransactionDTO.walletID is required
 
+        // With the walletID, find the wallet and charge to it
+        if (authorityService.getCurrentAuthority().equals(AuthorityName.CUSTOMER.name())) {
+            FakeEWallet wallet = fakeEWalletRepository.findById(createWithdrawTransactionDTO.getWalletID()).orElseThrow(EntityNotFoundException::new);
+            wallet.setBalance(wallet.getBalance() + createWithdrawTransactionDTO.getWithdrawAmount());
+            withdrawTransaction.setWallet(wallet);
+            fakeEWalletRepository.save(wallet);
+        }
         ((Withdrawable) customerAccount).withdraw(createWithdrawTransactionDTO.getWithdrawAmount());
-
-        wallet.setBalance(wallet.getBalance() + createWithdrawTransactionDTO.getWithdrawAmount());
-
-        withdrawTransaction.setWallet(wallet);
-
-        fakeEWalletRepository.save(wallet);
         return abstractTransactionRepository.save(withdrawTransaction);
     }
 

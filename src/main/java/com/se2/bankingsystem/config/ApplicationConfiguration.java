d51b215763e774ddf90c229334b6ad62ab8c0e65
Nguyen Thi Thuy Duong <nguyenthuyduong88hanu@gmail.com>
2021-05-08 16:37:13 +0700

    Test cases added, bug fixed

--- src/main/java/com/se2/bankingsystem/config/ApplicationConfiguration.java
+++ src/main/java/com/se2/bankingsystem/config/ApplicationConfiguration.java
@@ -5,10 +5,17 @@ import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
 @EnableScheduling
-public class ApplicationConfiguration {
+@EnableWebMvc
+public class ApplicationConfiguration implements WebMvcConfigurer {
 
     @Bean
     public ModelMapper modelMapper() {
@@ -16,5 +23,27 @@ public class ApplicationConfiguration {
         modelMapper.getConfiguration()
             .setMatchingStrategy(MatchingStrategies.STRICT);
         return modelMapper;
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE").allowedOrigins("*")
+            .allowedHeaders("*");
+    }
+
+    @Bean
+    public InternalResourceViewResolver defaultViewResolver() {
+        InternalResourceViewResolver bean = new InternalResourceViewResolver();
+        bean.setViewClass(JstlView.class);
+        bean.setPrefix("/WEB-INF/jsp/");
+        bean.setSuffix(".jsp");
+        return bean;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+            .addResourceHandler("/webjars/**", "/resources/**")
+            .addResourceLocations("/webjars/", "/resources/");
     }
 }
